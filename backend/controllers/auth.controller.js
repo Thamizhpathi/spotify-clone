f72375204f36897dbf3e46376addb9ea9dc13909
Thamizhpathi <tamizh401@gmail.com>
2025-04-14 21:32:20 +0530

    validate the user inputs in frontend and manipute the error msg and made navbar responsive

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,6 +22,7 @@ export const signup= async(req,res,next)=>{
 
     }catch(error){
         next(error)
+        console.log(error)
     }
     
 }
@@ -33,7 +34,7 @@ export const signin= async(req,res,next)=>{
 
         if(!checkUser){
             const error=new Error("user not found")
-            res.status=404
+            error.status=404
             throw error
         }
         const passwordValid= await bcrypt.compare(password,checkUser.password)
@@ -41,17 +42,16 @@ export const signin= async(req,res,next)=>{
         if(!passwordValid){
             
             const error=new Error("password not valid")
-            res.status=401
+            error.status=401
             throw error
         }
         res.status(200).json({message:"user loggedin successfully",data:checkUser})
 
     }
     catch(error){
-        res
-        .status(error.status || 500)
-        .json({ message: error.message || "Signup failed" });
-        console.log(error.message)
+    
+      next(error)
+      
     }
 
 
